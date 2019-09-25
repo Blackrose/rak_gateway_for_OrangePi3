023951902c33f7e1ad96d6c0c16e96db3bd73095
ZhuQI <qi.zhu@rakwireless.com>
2019-09-25 11:54:42 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # rak_gateway_for_OrangePi3
+
+##	Introduction 
+
+The aim of this project is to help users set up a LoRa network easily on OrangePi3.
+You can view a more detailed tutorial in doc/rak_gateway_for_OrangePi3.docx.
+
+##	Supported platforms
+
+This project currently provides support for the below platforms.
+* RAK831
+* RAK833
+* RAK2247
+
+##	Changelog
+
+2019-09-25 V1.0.0
+
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Stop on the first sign of trouble
+set -e
+
+if [ $UID != 0 ]; then
+    echo "ERROR: Operation not permitted. Forgot sudo?"
+    exit 1
+fi
+
+SCRIPT_COMMON_FILE=$(pwd)/rak/rak/shell_script/rak_common.sh
+source $SCRIPT_COMMON_FILE
+
+# select gw model
+./choose_model.sh
+
+apt update -y
+pushd rak
+./install.sh
+sleep 1
+popd
+
+pushd loraserver
+./install.sh
+sleep 1
+popd
+
+pushd lora
+./install.sh
+sleep 1
+
+echo_success "*********************************************************"
+echo_success "*  The RAKwireless gateway is successfully installed!   *"
+echo_success "*********************************************************"
